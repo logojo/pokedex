386ab410ea0fcdd0da5b991ade77d3d6d715a51b
logojo <joel_88_7@hotmail.com>
2024-04-27 14:32:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+
+#Ejecutar en desarrollo
+
+1. clonar el repositorio
+2. ejecutar
+
+´´´
+npm install
+´´´
+
+3. tener Nest CLI instalado
+
+´´´
+npm i -g @nestjs/cli
+´´´
+
+4. Levantar la base de datos
+
+´´´
+docker-compose up -d
+´´´
+
+##Stack usado
+*MongoDB
+*NestJS
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,64 @@
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model, isValidObjectId } from 'mongoose';
+
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+    @InjectModel( Pokemon.name)
+    private readonly pokemonModel : Model<Pokemon> 
+  ) { }
+
+  async create(createPokemonDto: CreatePokemonDto) {
+   try {
+    const pokemon = await this.pokemonModel.create( createPokemonDto );
+    return pokemon;
+   } catch (error) {
+      if ( error.code === 11000 )
+           throw new BadRequestException( `Pokemon already exist ${ JSON.stringify( error.keyValue )}` )
+
+      console.log( error );
+      throw new InternalServerErrorException('Upss something went wrong, check your server logs')
+   }
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  async findOne(term: string ) {
+    let pokemon : Pokemon;
+
+    if( !isNaN(+term) ) {
+        pokemon = await this.pokemonModel.findOne({no : term});
+    }
+
+    if( isValidObjectId( term ) ) 
+      pokemon = await this.pokemonModel.findById( term );
+
+    if( !pokemon ) 
+        pokemon = await this.pokemonModel.findOne({name : term.toLowerCase()});
+  
+
+    if( !pokemon ) {
+      throw new NotFoundException(`Pokemon with id, name or no "${ term }" not found `)
+    }
+
+    return pokemon;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${id} pokemon`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pokemon`;
+  }
+}
